51ccf958198decc2307f210683da381701d4ba81
kimko <djkimko@gmail.com>
2017-12-08 09:09:44 +0100

    adding facebook user info

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+//Import bot dependencies
+import { RetrieveUserProfile } from 'botbuilder-facebookextension';
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -21,6 +24,12 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
     (session, args, next) => {
-        session.send('You said: ' + session.message.text + ' which was ' + session.message.text.length + ' characters');
+        session.send('Hi ${session.userData.first_name} ${session.userData.last_name}! ' + 'You said: ' + session.message.text + ' which was ' + session.message.text.length + ' characters');
     }
 ]);
+
+bot.use(  
+    RetrieveUserProfile({
+        accessToken: process.env.FacebookAccessToken,
+    })
+);
